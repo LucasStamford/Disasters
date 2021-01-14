8c3848c910e792b51d74e31498f5b1a7f3b20885
LucasStamford <36779371+LucasStamford@users.noreply.github.com>
2021-01-13 23:43:53 -0300

    Add files via upload

--- disasters.py
+++ disasters.py
@@ -1,13 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-@author: Stamford
+DISASTERS
+
+https://www.kaggle.com/c/nlp-getting-started
+
+id       - a unique identifier for each tweet
+text     - the text of the tweet
+location - the location the tweet was sent from (may be blank)
+keyword  - a particular keyword from the tweet (may be blank)
+target   - in train.csv only, this denotes whether a tweet is about a real disaster (1) or not (0)
+
 """
+#IMPORTS
+#preprocessing
 import os
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+#model selection
+from sklearn.model_selection import GridSearchCV
+#models
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import xgboost
+import lightgbm as lgb
+#metrics
+from sklearn.metrics import roc_auc_score
+#plot
+import matplotlib as plt
 
+#Select Directory
 os.chdir('C:\Datasets\disasters')
+
+#Load Data
 ds = pd.read_csv('tweets.csv')
 
+#Pre-processing
 ds['text'] = ds['text'].str.replace(' (http.*)','')
 ds['text'] = ds['text'].str.replace('(#\w*) ','')
 ds['text'] = ds['text'].str.replace('\W',' ')
@@ -16,51 +48,35 @@ ds['text'] = ds['text'].str.replace(' (\d*) ', '')
 ds['text'] = ds['text'].str.replace('\s+',' ').str.strip()
 ds['text'] = ds['text'].str.lower()
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 vectorizer = TfidfVectorizer(ngram_range = (1,1), max_df=0.95, min_df=0.0005, stop_words = 'english')
 vet = vectorizer.fit_transform(ds['text'])
 X = pd.DataFrame(vet.A, columns=vectorizer.get_feature_names())
 
-from sklearn.model_selection import train_test_split
+#Split train and test data
 X_train, X_test, y_train, y_test = train_test_split(X, ds['target'], test_size=0.3, random_state=3, stratify=ds['target'])
 
-from sklearn.model_selection import GridSearchCV
-
+#Append models
 models = []
 name_models = []
 roc_auc = []
 
-from sklearn.naive_bayes import GaussianNB
 naive = GaussianNB()
-naive.fit(X_train, y_train)
-name_models.append(type(naive).__name__)
 models.append(naive)
 
-from sklearn.naive_bayes import MultinomialNB
 NB = MultinomialNB()
-NB.fit(X_train, y_train)
-name_models.append(type(NB).__name__)
 models.append(NB)
 
-from sklearn.linear_model import LinearRegression
 linear = LinearRegression(n_jobs=-1)
-linear.fit(X_train, y_train)
-name_models.append(type(linear).__name__)
 models.append(linear)
 
-from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier()
 param_grid = {
     'criterion':['gini','entropy'],
     'max_depth':[4,5,6,8,10,20,40,80,100]
     }
 grid = GridSearchCV(tree, param_grid=param_grid, cv=10, scoring='roc_auc')
-grid.fit(X_train, y_train)
-name_models.append(type(tree).__name__)
 models.append(grid)
 
-from sklearn.svm import SVC
 svc = SVC()
 param_grid = {
     'C':[1,10,100,1000],
@@ -68,11 +84,8 @@ param_grid = {
     'kernel':['linear','rbf']
     }
 grid = GridSearchCV(svc, param_grid=param_grid, cv=10, scoring='roc_auc')
-grid.fit(X_train, y_train)
-name_models.append(type(svc).__name__)
 models.append(grid)
 
-from sklearn.ensemble import RandomForestClassifier
 random = RandomForestClassifier()
 param_grid = {
     'bootstrap': [True, False],
@@ -83,12 +96,9 @@ param_grid = {
     'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
     }
 grid = GridSearchCV(random, param_grid=param_grid, cv=10, scoring='roc_auc')
-grid.fit(X_train, y_train)
-name_models.append(type(random).__name__)
 models.append(grid)
 
-import xgboost
-xgb = xgboost.XGBClassifier(objective='binary:logistic', tree_method='gpu_hist',gpu_id=1)
+xgb = xgboost.XGBClassifier(objective='binary:logistic', tree_method='gpu_hist')
 param_grid = {
         'min_child_weight': [1, 5, 10],
         'gamma': [0.5, 1, 1.5, 2, 5],
@@ -97,11 +107,8 @@ param_grid = {
         'max_depth': [3, 4, 5]
         }
 grid = GridSearchCV(xgb, param_grid=param_grid, cv=10, scoring='roc_auc')
-grid.fit(X_train, y_train)
-name_models.append(type(xgb).__name__)
 models.append(grid)
 
-import lightgbm as lgb
 gbm = lgb.LGBMClassifier(metric='auc', objective='binary', verbose=1)
 param_grid = {
     'num_leaves': [64, 128, 256],
@@ -111,16 +118,16 @@ param_grid = {
     'lambda_l2': [0, 1]
     }
 grid = GridSearchCV(gbm, param_grid=param_grid, cv=10, scoring='roc_auc')
-grid.fit(X_train, y_train)
-name_models.append(type(gbm).__name__)
 models.append(grid)
 
-from sklearn.metrics import roc_auc_score
+#Fit, Predict and Metrics
 for model in models:
+    model.fit(X_train, y_train)
+    name_models.append(type(model).__name__)
     y_predict = model.predict(X_test)
-    roc_auc.append(roc_auc_score(y_test, y_predict))
+    roc_auc = roc_auc_score(y_test, y_predict)
 
-import matplotlib.pyplot as plt
+#Plot Metrics
 plt.bar(name_models, roc_auc, color='blue')
 plt.xticks(name_models)
 plt.ylabel('ROC Curve')
